a57bd85cafc79594a85a595a2e48adc99c774a5e
abideen11 <abideenkareem17@gmail.com>
2019-09-13 10:46:24 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -17,6 +17,7 @@ function render(){
     let container = document.getElementById('container');
     container.textContent = state.count;
 }
+dispatch({ type: '@@INIT' })
 let button = document.getElementById('button');
 button.addEventListener('click', function(){
     dispatch({type: 'INCREASE_COUNT'})
